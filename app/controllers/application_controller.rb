7c074f63089734ae24f97fb07ac90e909e9f6b2d
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-14 21:01:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,24 +11,6 @@ get '/' do
   redirect to "/articles"
 end
 
-get '/articles' do
-  @articles = Article.all
-  erb :index
-end
-
-get '/articles/new' do
-  @article = Article.new
-  erb :new
-end
-
-post '/articles' do
-  @article = Article.create(params)
-  redirect to "/articles/#{@article.id}"
-end
-
-get '/articles/:id' do
-  @article = Article.find(params[:id])
-  erb :show
-end
+x
 
 end
