a0606a6ac33b09c1a742202a449b889c8ec02be3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-14 20:50:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,31 +7,4 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-  end
-#CREATE
-  get '/articles/new' do
-    @article = Article.new
-    erb :new
-  end
-
-  get '/articles' do
-    @articles = Article.all
-    erb :index
-  end
-
-  post '/articles' do
-    @article = Article.create(params)
-    redirect to "/articles/#{@article.id}"
-  end
-
-  #READ
-
-  get '/articles/:id' do
-    @article = Article.find(params[:id])
-    erb :show
-  end
-
-
-
 end
