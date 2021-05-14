1b9adf869be82d3ca0d87f59cec211a5f7a79e26
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-14 22:12:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,10 @@ get '/articles/:id/edit' do
   erb :edit
 end
 
-patch '/articles/:id'
+patch '/articles/:id' do
+  @article = Article.find(params[:id])
+  @article.update(params[:article])
+  redirect to "/articles/#{@article.id}"
 end
 
 end
