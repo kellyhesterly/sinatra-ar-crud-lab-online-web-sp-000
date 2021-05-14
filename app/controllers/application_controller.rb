273a87a54a605377a49c69e16a68913c35047131
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-14 20:13:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     @article = Article.create(params)
-    redirect to "/articles/#{@article.id}"
   end
 
   get '/articles' do
